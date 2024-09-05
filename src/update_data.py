87b58e9a9e98610b30bfc269498634b747134399
Pipe Galera <pipegalera@gmail.com>
2024-09-05 11:45:45 +0200

    speeded up consolidated file creation

--- src/update_data.py
+++ src/update_data.py
@@ -3,7 +3,6 @@ import os
 from datetime import datetime, timezone, timedelta
 from dotenv import load_dotenv
 load_dotenv()
-from utils import spy_symbols
 from pytz import timezone
 from alpaca.data.historical import StockHistoricalDataClient
 from alpaca.data.timeframe import TimeFrame, TimeFrameUnit
@@ -15,9 +14,8 @@ ALPACA_KEY = os.getenv("ALPACA_KEY")
 ALPACA_SECRET = os.getenv("ALPACA_SECRET")
 DATA_PATH = os.getenv("DATA_PATH")
 
-
 timeframe=TimeFrame(1, TimeFrameUnit.Minute)
-start_date = datetime.now(timezone('US/Eastern')) - timedelta(days=1)
+start_date = datetime.now(timezone('US/Eastern')) - timedelta(hours=15)
 
 def get_updated_stock_data(symbols):
     data_client = StockHistoricalDataClient(ALPACA_KEY, ALPACA_SECRET)
@@ -32,35 +30,39 @@ def get_updated_stock_data(symbols):
     return df
 
 def save_updated_stock_data(df):
-    # Get the symbols
-    symbols = df.reset_index()["symbol"].unique()
-    print(f"Updating the parquet files and SQL database with the new data...")
+
+    print(f"Updating the CSV files with the new data...")
     start_time = time.time()
+
+    # Update each symbol CSV file
+    symbols = df.reset_index()["symbol"].unique()
+    consolidated_file_name = '503 S&P Symbols - All data'
     for symbol in symbols:
         print(f"Updating: {symbol}...")
         combined_data = (f'''
-            WITH combined_data AS (
-                SELECT * FROM df WHERE symbol = '{symbol}'
-                UNION
-                SELECT * FROM '{DATA_PATH}parquet_files/{symbol}.parquet'
-                ORDER BY timestamp
-            )
-            SELECT * FROM combined_data
-        ''')
-        # Write over every parquet file
+                   WITH combined_data AS (
+                       SELECT * FROM df WHERE symbol = '{symbol}'
+                       UNION
+                       SELECT * FROM '{DATA_PATH}/{symbol}.csv'
+                       ORDER BY timestamp
+                   )
+                   SELECT * FROM combined_data
+               ''')
+        # Write over every CSV file
         duckdb.query(f'''
-            COPY ({combined_data})
-            TO '{DATA_PATH}parquet_files/{symbol}.parquet'
-            (FORMAT 'parquet')
-        ''')
+                COPY ({combined_data})
+                TO '{DATA_PATH}/{symbol}.csv'
+                (FORMAT 'CSV', HEADER)
+            ''')
 
-        # Write over the general parquet file
-        duckdb.query(f'''
-            COPY ({combined_data})
-            TO '{DATA_PATH}parquet_files/spy_intraday_1M.parquet'
-            (FORMAT 'parquet')
-        ''')
-
+    # Update the consolidated CSV file
+    print(f"Updating the consolidated CSV file with all the data...")
+    duckdb.query(f"""
+    COPY (
+        SELECT * FROM read_csv_auto('{DATA_PATH}/*.csv')
+    ) TO '{DATA_PATH}/503 S&P Symbols - All data.CSV'
+    WITH (FORMAT 'CSV', HEADER)
+    """)
     end_time = time.time()
     execution_time = round(end_time - start_time, 2)
 
@@ -69,8 +71,8 @@ def save_updated_stock_data(df):
     print(f"--> Timeframe added: {start_date} - Latest available")
 
 
-
 def main():
+    spy_symbols = [os.path.splitext(file)[0] for file in os.listdir(DATA_PATH) if file.endswith('.csv')]
     df = get_updated_stock_data(spy_symbols)
     save_updated_stock_data(df)
 
