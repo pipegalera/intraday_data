bcb8bc0f7b60de07f28a961896da2503b2cfb26a
Pipe Galera <pipegalera@gmail.com>
2024-09-13 15:15:55 +0200

    created exception to sorting symbols

--- app/main.py
+++ app/main.py
@@ -548,7 +548,7 @@ def get_symbols(search_query=None):
                 'modified_time': modified_time
             })
 
-    symbols.sort(key=lambda x: x['modified_time'])
+    symbols.sort(key=lambda x: (len(x['symbol']) <= 5, x['modified_time']))
 
     return symbols
 
